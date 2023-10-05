ef87b4d1180fae17fdbf03c08bbac79e55b0a804
unknown <you@example.com>
2023-10-05 01:12:55 -0700

    initial commit set up project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const app = express()
+// to read the file .env and put those environment variable in the global variable process.rnv
+require("dotenv").config()
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@e-commerce.psemudv.mongodb.net/?retryWrites=true&w=majority`
+
+/*we use mongoose as a third party libruary to connect to the data base*/
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("connected to db");
+  })
+  .catch((err) => console.log("hadi erreur t3 db connection ", err));
+
+app.listen(process.env.PORT,()=>console.log('server is listning'))
