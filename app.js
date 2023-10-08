fd363223eeafed6211ba3d6490c69f187d917e5a
karim iarkane <karimiarkane6@gmail.com>
2023-10-07 17:44:53 -0700

    fix the setup and put it in different file

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+// this require is for executing the dbconnection code and get the mongoose variable
 const mongoose = require('./config/dbConnection')
 const app = express()
+
+app.get("/",(req,res)=>{
+  res.send("hello")
+})
 
 app.listen(process.env.PORT ||4000,()=>console.log('server is listning'))
